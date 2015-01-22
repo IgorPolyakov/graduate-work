11b8bef9233f66dd7aaf7a0a7df87e7f32034864
Igor Polyakov <fox.user.3@gmail.com>
2015-01-22 17:37:25 +0600

    func from Matlab: Coder

---
+++ 3tdparty/conv2.h
@@ -0,0 +1,25 @@
+/*
+ * conv2.h
+ *
+ * Code generation for function 'conv2'
+ *
+ * C source code generated on: Wed Jan 21 21:50:20 2015
+ *
+ */
+
+#ifndef __CONV2_H__
+#define __CONV2_H__
+/* Include files */
+#include <math.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rt_nonfinite.h"
+
+#include "rtwtypes.h"
+#include "lucas_kanade_matlab_types.h"
+
+/* Function Declarations */
+extern void conv2(const real_T arg1[10000], const real_T arg2[4], real_T c[10201]);
+#endif
+/* End of code generation (conv2.h) */
