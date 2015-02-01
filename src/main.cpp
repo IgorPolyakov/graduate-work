4f7c31ea70976296874366f3e9def235f1228d7c
Igor Polyakov <fox.user.3@gmail.com>
2015-02-01 14:43:51 +0600

    fix option

--- src/main.cpp
+++ src/main.cpp
@@ -61,11 +61,11 @@ int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
     QImage firstImg,secondImg,outImg;
-    /*if(argc == 0)
+    if (optarg == NULL)
     {
-        std::cout << "App: пропущены операнды, задающие входные файлы\nПо команде «app -h» можно получить дополнительную информацию.";
-        return (-1);
-    }*/
+        //std::cout << "LukasKanadeQt: пропущены операнды, задающие входные файлы\nПо команде «lukas_kanade_qt -h» можно получить дополнительную информацию.";
+        //return(0);
+    }
     int pr = 0;
     while((pr = getopt(argc, argv, "l:r:vhd")) != -1)
     {
@@ -86,22 +86,27 @@ int main(int argc, char *argv[])
                 qDebug() << "S Image path" << optarg;
                 break;
             case 'v':
-                //QString version(GIT_VERSION);
-                std::cout << "Version: " << GITHASH << "\n";
+                std::cout << "LukasKanadeQt version: " << GITHASH << "\n";
                 return(0);
             case 'h':
-                std::cout << "Usage ./binary -l <First image> -o <Second image>\n";
-                break;
+                std::cout << "\nNAME: \n\tLukasKanadeQt \n\tUsage to EXEC ./lukas_kanade_qt -l <First image> -o <Second image>\n";
+                std::cout << "\nDESCRIPTION:";
+                std::cout << "\n\tApplication created in order to write a graduate work on specialty 220301\n";
+                std::cout << "\n\t-l\n\t\t load left image";
+                std::cout << "\n\t-r\n\t\t load right image";
+                std::cout << "\n\t-v\n\t\t show version";
+                std::cout << "\n\t-h\n\t\t show help";
+                std::cout << "\n\t-d\n\t\t debug mod on\n";
+                return(0);
             case 'd':
                 std::cout << "Debug mode ON" << "\n";
-            default:
-                std::cout << "App: пропущены операнды, задающие входные файлы\nПо команде «app -h» можно получить дополнительную информацию.";
                 break;
         }
     }
     firstImg  =  firstImg.convertToFormat(QImage::Format_ARGB32);
     imageInfo(firstImg);
     secondImg = secondImg.convertToFormat(QImage::Format_ARGB32);
+    imageInfo(secondImg);
     outImg = outImg.convertToFormat(QImage::Format_ARGB32);
 
     int** pArr = getArrBright(firstImg);
