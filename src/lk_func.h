f59c7018b24ee221660486c695a516dc3567c00a
IgorPolyakov <fox.user.3@gmail.com>
2015-04-15 18:19:46 +0600

    fix(error) Memory leak: data; fix(error) Memory leak: vectorOptFlw; rename defines;

--- src/lk_func.h
+++ src/lk_func.h
@@ -30,7 +30,7 @@ QImage computeGrid(QImage image, int** arrGrayPrevious, int** arrGrayNext);
  * \param [in] arrGrayNext − указатель на массив яркостей второго кадра
  * \return вектор оптического потока
  */
-double* computeOptFlow(subSize* initialWindow, int** arrGrayPrevious, int** arrGrayNext);
+void computeOptFlow(double *shiftVectr, subSize* initialWindow, int** arrGrayPrevious, int** arrGrayNext);
 
 /*! Произведение матрицы на вектор.
  * \brief multiplicMtrxAndVectr
