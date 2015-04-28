7942f94be10cb7cefdf7faca0bd0fe48ed8d4fcd
IgorPolyakov <fox.user.3@gmail.com>
2015-04-28 16:01:01 +0600

    fixed code documentation; save vector to vf field; train commit

--- src/lk_func.cpp
+++ src/lk_func.cpp
@@ -9,129 +9,109 @@
 #define RESIZE 5
 #define SIZE_MAT_INV 2
 
+/*!
+ * \brief inversion - Нахождения обратной матрицы.
+ * \param [in] **A − Указатель на массив
+ * \param [in] N − Его размер
+ */
 void inversion(double **A, int N)
 {
     double temp;
-
     double **E = new double *[N];
-
     for (int i = 0; i < N; i++)
         E[i] = new double [N];
-
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++) {
             E[i][j] = 0.0;
-
             if (i == j)
                 E[i][j] = 1.0;
         }
-
     for (int k = 0; k < N; k++) {
         temp = A[k][k];
-
         for (int j = 0; j < N; j++) {
             A[k][j] /= temp;
             E[k][j] /= temp;
         }
-
         for (int i = k + 1; i < N; i++) {
             temp = A[i][k];
-
             for (int j = 0; j < N; j++) {
                 A[i][j] -= A[k][j] * temp;
                 E[i][j] -= E[k][j] * temp;
             }
         }
     }
-
     for (int k = N - 1; k > 0; k--) {
         for (int i = k - 1; i >= 0; i--) {
             temp = A[i][k];
-
             for (int j = 0; j < N; j++) {
                 A[i][j] -= A[k][j] * temp;
                 E[i][j] -= E[k][j] * temp;
             }
         }
     }
-
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             A[i][j] = E[i][j];
-
     for (int i = 0; i < N; i++)
         delete [] E[i];
-
     delete [] E;
 }
 
-int **getArrBright(QImage image)
-{
-    int gray;
-    //get some space for array of bright
-    int** ary = new int*[image.width()];
-    for (int i = 0; i < image.width(); ++i)
-        ary[i] = new int[image.height()];
-    //convert color image to grayscale
-    for (int i = 0; i < image.width(); i++) {
-        for (int j = 0; j < image.height(); j++) {
-            gray = qGray(image.pixel(i, j));
-            ary[i][j] = gray;
-        }
-    }
-    return ary;
-}
-
-QImage computeGrid(Data2Db* leftImg, Data2Db* rightImg, Data2Db* out1Img, QImage outImg)
+/*!
+ * \brief computeGrid - Строит сетку по верх изображения, в точках
+ * пересечения ищется вектор оптического потока
+ * \param [in] leftImg - указатель на массив яркостей первого кадра
+ * \param [in] rightImg - указатель на массив яркостей второго кадра
+ * \return двумерный массив содержащий векторное поле, в формате VF
+ */
+VF2d* computeGrid(Data2Db* leftImg, Data2Db* rightImg)
 {
     subSize* kernel = new subSize;
-
-    double* vectorOptFlw = new double[SIZE_MAT_INV];
+    /*double* vf= new double[SIZE_MAT_INV];
     for (int i = 0; i < SIZE_MAT_INV; ++i) {
-        vectorOptFlw[i] = 1.0;
-    }
-
+        vf[i] = 1.0;
+    }*/
     kernel->rc = g_sizeWindowSeach;
     kernel->step = g_stepForGrid;
-
     int Vcx = (leftImg->cx()-(2*kernel->rc)-1)/g_stepForGrid;
     int Vcy = (leftImg->cy()-(2*kernel->rc)-1)/g_stepForGrid;
-
-    QPainter painter(&outImg);
-    painter.setPen(QPen(Qt::red));
+    static VF2d *vf = new VF2d(Vcx, Vcy, 1,1,0,0, DV_ALIGNMENT);
+    /*
+     *Data2D_(int cx, int cy, int gx=1, int gy=1, int ox=0, int oy=0, int align = DV_ALIGNMENT)
+    : ProtoData2D(DataType<T_>::depth, DataType<T_>::fmt, DataType<T_>::channels, 0, "", cx, cy, gx, gy, ox, oy, align) {}
+    */
     for (int i = kernel->rc+1; i < (leftImg->cx()-kernel->rc); i += Vcy) {
         kernel->cx = i;
-        for (int j = kernel->rc+1; (j < (leftImg->cy()-kernel->rc)); j += Vcx) {
+        for (int j = kernel->rc+1; (j < (leftImg->cy()-kernel->rc));
+             j += Vcx) {
             kernel->cy = j;
-            vectorOptFlw = computeOptFlow(kernel, leftImg, rightImg);
-            //computeOptFlow(&vectorOptFlw, initialWindow, arrGrayPrevious, arrGrayNext);
-            painter.drawLine(kernel->cx, kernel->cy, kernel->cx + vectorOptFlw[0], kernel->cy + vectorOptFlw[1]);
+            //vf = (VF2d)computeOptFlow(kernel, leftImg, rightImg);
         }
     }
-    delete[] vectorOptFlw;
     delete kernel;
-    outImg.save(g_outputFolder + "/879897.png");
-    return outImg;
+    return vf;
 }
 
+/*!
+ * \brief computeOptFlow - Вычисление вектора оптического потока
+ * \param [in] kernel - структура содержащая сведения о местонахождении пикселя, размерах окна поиска и прочего
+ * \param [in] leftImg - указатель на массив яркостей первого кадра
+ * \param [in] rightImg - указатель на массив яркостей второго кадра
+ * \return вектор оптического потока
+ */
 double* computeOptFlow(subSize* kernel, Data2Db* leftImg, Data2Db* rightImg)
-//void computeOptFlow(double* shiftVectr, subSize* kernel, int** arrGrayPrevious, int** arrGrayNext)
 {
     double iY = 0.0,   iX = 0.0,   iTX = 0.0, iTY = 0.0, iXY = 0.0;
     double tmpX, tmpY, tmpT;
     double* shiftVectr = new double[2];
-
     for (int i = 0; i < 2; ++i) {
         shiftVectr[i] = 0.0;
     }
     int deltaX = 0;
     int deltaY = 0;
-
     double **A = new double *[SIZE_MAT_INV];
-
     for (int i = 0; i < SIZE_MAT_INV; i++)
         A[i] = new double [SIZE_MAT_INV];
-
     int* b = new int [SIZE_MAT_INV];
 
     if (g_isDebug) qDebug() << "SubSize:X" << kernel->cx << "Y:" << kernel->cy << "R:" << kernel->rc << "\n";
@@ -154,10 +134,8 @@ double* computeOptFlow(subSize* kernel, Data2Db* leftImg, Data2Db* rightImg)
         A[0][1] = iXY;
         A[1][0] = iXY;
         A[1][1] = iY;
-
         b[0] = -iTX;
         b[1] = -iTY;
-
         inversion(A, SIZE_MAT_INV);
         shiftVectr = multiplicMtrxAndVectr(A, b);
 
@@ -173,7 +151,8 @@ double* computeOptFlow(subSize* kernel, Data2Db* leftImg, Data2Db* rightImg)
         if (kernel->cy + shiftVectr[1] - kernel->rc < 0) {
             break;
         }
-        if ((shiftVectr[0] == shiftVectr[0]) || (shiftVectr[1] == shiftVectr[1])) { //NaN Checking
+        if ((shiftVectr[0] == shiftVectr[0])
+            || (shiftVectr[1] == shiftVectr[1])) { //NaN Checking
             deltaX = (int)floor(shiftVectr[0]);
             deltaY = (int)floor(shiftVectr[1]);
         } else {
@@ -182,26 +161,22 @@ double* computeOptFlow(subSize* kernel, Data2Db* leftImg, Data2Db* rightImg)
             shiftVectr[1] = 0.0;
         }
     }
-
     if (g_isDebug) qDebug() << shiftVectr[0] << shiftVectr[1] << "return";
-    freeMemoryFloat(A, SIZE_MAT_INV);
+
+    for (int i = 0; i < SIZE_MAT_INV; ++i)
+        delete[] A[i];
+    delete [] A;
+
     delete[] b;
     return shiftVectr;
 }
-void freeMemoryInt(int** trash, int size)
-{
-    for (int i = 0; i < size; ++i)
-        delete[] trash[i];
-    delete [] trash;
-}
 
-void freeMemoryFloat(double** trash, int size)
-{
-    for (int i = 0; i < size; ++i)
-        delete[] trash[i];
-    delete [] trash;
-}
-
+/*!
+ * \brief multiplicMtrxAndVectr - Произведение матрицы на вектор
+ * \param [in] **array − указатель на массив
+ * \param [in] *vector − указатель на вектор
+ * \return  *tmp − результат произведения
+ */
 double* multiplicMtrxAndVectr(double** array, int* vector)
 {
     double* tmp = new double[SIZE_MAT_INV];
@@ -216,15 +191,30 @@ double* multiplicMtrxAndVectr(double** array, int* vector)
     return tmp;
 }
 
+/*!
+ * \brief getImageInfo - Получение информации о входном изображении
+ * \param [in] image − Изображение
+ * \param [in] path − Путь к нему
+ */
 void getImageInfo(imageInform* image, QString path)
 {
     QFileInfo fileImage(path);
-    qDebug() << "About image " << fileImage.fileName() << " :: FileSize:" << fileImage.size() << "bytes. Size: " << image->height << "x" << image->width;
+    qDebug() << "About image " << fileImage.fileName() << " :: FileSize:"
+             << fileImage.size() << "bytes. Size: " << image->height << "x" <<
+             image->width;
 }
 
+/*!
+ * \brief joinImage - Объединение трёх изображений(первого, второго и первого с нанесённым поверх векторным полем)
+ * \param [in] img1 − Первое изображение
+ * \param [in] img2 − Второе изображение
+ * \param [in] img3 − Первого с нанесённым поверх векторным полем
+ * \param [in] info − Имя сохраняемого файла
+ */
 void joinImage(QImage img1, QImage img2, QImage img3, QString info)
 {
-    QImage result(img1.width() + img2.width() + img3.width(), img1.height(), QImage::Format_ARGB32);
+    QImage result(img1.width() + img2.width() + img3.width(),
+                  img1.height(), QImage::Format_ARGB32);
     QPainter paint;
     paint.begin(&result);
     paint.drawImage(0, 0, img1);
@@ -233,19 +223,23 @@ void joinImage(QImage img1, QImage img2, QImage img3, QString info)
     result.save(g_outputFolder + "/" + info + ".png");
 }
 
+/*!
+ * \brief resizeImage - Функция масштабирования изображения, для построения пирамиды уменьшенных изображений
+ * \param [in] image - структура содержащая сведения о размерах масштабируемого изображения
+ * \param [in] arrGrayPrevious − Указатель на массив яркостей первого кадра
+ * \param [in] kK − Коэффициент уменьшения изображения
+ * \return [out] - указатель на массив масштабированных изображений
+ */
 int* resizeImage(imageInform* image, int** arrGrayPrevious, int kK)
 {
     /*if((image.width()%2 == 0)||(image.height()%2 == 0))*/
     int newWidth = (image->width/kK);
     int newHeight= (image->height/kK);
     int tmp = 0;
-
     int* ptmpImg = new int[newHeight * newWidth];
     int** data = new int*[newHeight];
-
     for(int i = 0; i < newHeight; ++i)
         data[i] = ptmpImg + newWidth * i;
-
     for (int i = 0; i < newWidth; i++) {
         for (int j = 0; j < newHeight; j++) {
             for (int ii = 0; ii < kK-1; ++ii) {
@@ -257,8 +251,8 @@ int* resizeImage(imageInform* image, int** arrGrayPrevious, int kK)
             tmp = 0;
         }
     }
-
-    QImage result((uchar*)ptmpImg, newWidth, newHeight, QImage::Format_RGB32);
+    QImage result((uchar*)ptmpImg, newWidth, newHeight,
+                  QImage::Format_RGB32);
     QString s = QString::number(kK);
     result.save(g_outputFolder + "/resize" + s + ".png");
     //freeMemoryInt(ptmpImg, newWidth);
@@ -266,15 +260,31 @@ int* resizeImage(imageInform* image, int** arrGrayPrevious, int kK)
     return ptmpImg;
 }
 
+/*!
+ * \brief getMemoryForPyramid - Для построения пирамиды, масштабированных изображений, нужно выделить память, чем эта функция и занимается
+ * \param [in] image − исходное изображение
+ * \param [in] arrGrayPrevious − указатель на массив яркостей первого кадра
+ */
 int** getMemoryForPyramid(imageInform* image, int** arrGrayPrevious)
 {
     int** pToPyramid = new int*[LVL_PYRAMID];
     for (int i = 0; i < LVL_PYRAMID; ++i)
         pToPyramid[i] = new int[(image->height * image->width)/RESIZE];
-
     for (int i = 0; i < LVL_PYRAMID; ++i) {
         pToPyramid[i] = resizeImage(image, arrGrayPrevious, RESIZE);
     }
     return pToPyramid;
 }
 
+/*!
+ * \brief saveVfResult
+ * \param vf
+ */
+void saveVfResult(VF2d vf/*, Data2Db img*/)
+{
+    /*if (WriteVF(g_outputFolder + "/" + "info" + ".png", VF2d* vf) == 0) {
+        qDebug() << "Correct write data";
+    } else {
+        qDebug() << "Error : can't write vf data";
+    }*/
+}
--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 #include "version.h"
 int main(int argc, char *argv[])
 {
-    QImage leftImg, rightImg, outImg;
+    //QImage leftImg, rightImg, outImg;
     QString info, listfilename;
     /*
      * Default opt'arg
@@ -27,11 +27,11 @@ int main(int argc, char *argv[])
      * end
      */
     if (argc <= 1) {
-        qDebug() << "LukasKanadeQt: пропущены операнды, задающие входные файлы\nПо команде «lukas_kanade_qt -h» можно получить дополнительную информацию.\n";
+        qDebug() <<
+                 "LukasKanadeQt: пропущены операнды, задающие входные файлы\nПо команде «lukas_kanade_qt -h» можно получить дополнительную информацию.\n";
         return (0);
     }
     int pr = 0;
-
     while ((pr = getopt(argc, argv, "l:vhdi:w:g:o:")) != -1) {
         switch (pr) {
         case 'l':
@@ -40,12 +40,15 @@ int main(int argc, char *argv[])
         case 'v':
             qDebug() << "LukasKanadeQt version: " << VERSION ;
             qDebug() << "branch: " << PROJECT_GIT_REF ;
-            qDebug() << "build date: " << PROJECT_BUILD_DATE << " " << PROJECT_BUILD_TIME ;
+            qDebug() << "build date: " << PROJECT_BUILD_DATE << " " <<
+                     PROJECT_BUILD_TIME ;
             return (0);
         case 'h':
-            qDebug() << "\nNAME: \n\tLukasKanadeQt \n\tUsage to EXEC ./bin/lukas_kanade_qt  -l <list_of_image>";
+            qDebug() <<
+                     "\nNAME: \n\tLukasKanadeQt \n\tUsage to EXEC ./bin/lukas_kanade_qt  -l <list_of_image>";
             qDebug() << "\nDESCRIPTION:";
-            qDebug() << "\n\tApplication created in order to write a graduate work on specialty 220301\n";
+            qDebug() <<
+                     "\n\tApplication created in order to write a graduate work on specialty 220301\n";
             qDebug() << "\n\t-l\t\t load list path image";
             qDebug() << "\n\t-o\t\t output directory";
             qDebug() << "\n\t-i\t\t count iteration (1 by default)";
@@ -77,26 +80,21 @@ int main(int argc, char *argv[])
             break;
         }
     }
-
     QFile listfile(listfilename);
     if (!listfile.open(QIODevice::ReadOnly | QIODevice::Text)) {
         qDebug() << "Cannot load list image file\n";
         return (-1);
     }
-
     QStringList imagelist;
     QTextStream in(&listfile);
-
     static Data2Db *pLeftImg = 0;
     static Data2Db *pRightImg = 0;
-    static Data2Db *pOutImg = 0;
+    static VF2d *vf = 0;
 
     while (!in.atEnd())
         imagelist.append(in.readLine());
     listfile.close();
-
-    for (int i = 1, cnt = 0, ocnt = 0; i < imagelist.size(); i++)
-    {
+    for (int i = 1, cnt = 0, ocnt = 0; i < imagelist.size(); i++) {
         std::cout << 0 << "," << (100*i)/imagelist.size() << "," << std::endl;
         pLeftImg = ReadImage(imagelist[cnt].toLocal8Bit().data());
         /*if (!leftImg.load(imagelist[cnt].toLocal8Bit().data())) {
@@ -108,29 +106,22 @@ int main(int argc, char *argv[])
             qDebug() << "Cannot load " << i << "image file\n";
             return (-1);
         }*/
-        /*
-        leftImg = leftImg.convertToFormat(QImage::Format_ARGB32);
-        rightImg = rightImg.convertToFormat(QImage::Format_ARGB32);
-        */
-
-        /*int** pToLeftImg = getArrBright(leftImg);
-        int** pToRightImg = getArrBright(rightImg);*/
 
         QDir outDir(g_outputFolder);
         if (!outDir.exists()) {
             outDir.mkpath(".");
         }
-        std::cout << 15 << "," << (100*i)/imagelist.size() << "," << std::endl;
-
-        outImg = computeGrid(pLeftImg, pRightImg, pOutImg, leftImg);
-
-        std::cout << 75 << "," << (100*i)/imagelist.size() << "," << std::endl;
-
-        joinImage(leftImg, rightImg, outImg, info);
+        std::cout << 15 << "," << (100*i)/imagelist.size() << "," <<
+                  std::endl;
+        vf = computeGrid(pLeftImg, pRightImg);
+        std::cout << 75 << "," << (100*i)/imagelist.size() << "," <<
+                  std::endl;
+        //joinImage(leftImg, rightImg, outImg, info);
         //memory, now you free!
         /*freeMemoryInt(pToLeftImg, leftImg.width());
         freeMemoryInt(pToRightImg, rightImg.width());*/
-        std::cout << 100 << "," << (100*i)/imagelist.size() << "," << std::endl;
+        std::cout << 100 << "," << (100*i)/imagelist.size() << "," <<
+                  std::endl;
     }
     std::cout << 100 << "," << 100 << "," << std::endl;
     return 0;
