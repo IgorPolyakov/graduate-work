047307566918e050bcc51ce65cfc21fa81a7519c
IgorPolyakov <fox.user.3@gmail.com>
2015-05-05 16:49:15 +0600

    update clean.sh & update ChangeLog

--- clean.sh
+++ clean.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 OK=" ..... ok"
 
-echo -ne " * delete all 'bin'"
+echo -ne " * delete all 'folder'"
 rm -rf 'bin/'
 rm -rf 'output/'
 rm -rf 'obj/'
+rm -rf 'dv/'
 echo $OK
 
 echo -ne " * delete all 'Makefile'"
@@ -26,4 +27,8 @@ find . -iname '*.cmake' -delete
 find . -iname 'CMakeCache.txt' -delete
 find . -iname 'CMakeLists.txt.user' -delete
 find . -iname '*.pro.user' -delete
+find . -iname 'CMakeFiles' -delete
+find . -iname 'Makefile' -delete
+find . -iname 'cmake_install.cmake' -delete
+find . -iname 'install_manifest.txt' -delete
 echo $OK
