f877f958bdb9619d91f44ee55432692f22dfec6e
IgorPolyakov <fox.user.3@gmail.com>
2015-05-25 20:59:15 +0600

    doc(*): update doxygen comment

--- src/lk_interpolation.h
+++ src/lk_interpolation.h
@@ -1,8 +1,11 @@
 #ifndef LK_INTERPOLATION
 #define LK_INTERPOLATION
+/*!
+\file
+\brief Заголовочный файл с описанием функций. Данный файл содержит в себе определения функций отвечающих за интерполирование данных.
+*/
 double bilinearInterpolation (double delx, double dely, uchar q11, uchar q12, uchar q21, uchar q22, int x1, int y1);
 double cubicInterpolate     (double p[4], double x);
 double bicubicInterpolate   (double p[4][4], double x, double y);
-double tricubicInterpolate  (double p[4][4][4], double x, double y, double z);
 #endif // LK_INTERPOLATION
 
